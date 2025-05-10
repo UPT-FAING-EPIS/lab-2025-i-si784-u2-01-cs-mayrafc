97330cf4b9005229b4bb12c2f2ba44cbb5981916
mayrafc <mc2021072620@virtual.upt.pe>
2025-05-10 09:22:14 -0500

    lab primera parte

---
+++ MyMath/Math.Lib/Rooter.cs
@@ -0,0 +1,19 @@
+namespace Math.Lib
+{
+    public class Rooter
+    {
+        public double SquareRoot(double input)
+        {
+            if (input <= 0.0) throw new ArgumentOutOfRangeException();
+            double result = input;
+            double previousResult = -input;
+            while (System.Math.Abs(previousResult - result)
+                > result / 1000)
+            {
+            previousResult = result;
+            result = result - (result * result - input) / (2 * result);
+            }
+            return result;
+        }
+    }
+}
---
+++ MyMath/Math.Tests/RooterTests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Math.Lib;
+
+namespace Math.Tests
+{
+    [TestClass]
+    public class RooterTests
+    {
+        [TestMethod]
+        public void BasicRooterTest()
+        {
+            Rooter rooter = new Rooter();
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input);
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+
+        [TestMethod]
+        public void RooterTestNegativeInputx()
+        {
+            Rooter rooter = new Rooter();
+            try
+            {
+                rooter.SquareRoot(-10);
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                return;
+            }
+            Assert.Fail();
+        }
+    }
+}
