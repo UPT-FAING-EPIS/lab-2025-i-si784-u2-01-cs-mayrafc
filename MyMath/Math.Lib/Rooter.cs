fbb5b3128aea3b13968f6b01e6f5db1adb8f1f15
mayrafc <mc2021072620@virtual.upt.pe>
2025-05-24 09:42:01 -0500

    arreglo del rooter.cs

--- MyMath/Math.Lib/Rooter.cs
+++ MyMath/Math.Lib/Rooter.cs
@@ -18,12 +18,18 @@ namespace Math.Lib
         public double SquareRoot(double input)
         {
             if (input <= 0.0)
-                throw new ArgumentOutOfRangeException(nameof(input), "El valor ingresado es invalido, solo se puede ingresar números positivos.");
+                throw new ArgumentOutOfRangeException(nameof(input), "El valor ingresado es inválido, solo se puede ingresar números positivos.");
 
             double result = input;
             double previousResult = -input;
 
-            while (System.Math.Abs(previousResult - result) > result / 1000)
+            while (Math.Abs(previousResult - result) > result / 1000)
             {
                 previousResult = result;
-                result = result - (result
+                result = result - (result * result - input) / (2 * result);
+            }
+
+            return result;
+        }
+    }
+}
