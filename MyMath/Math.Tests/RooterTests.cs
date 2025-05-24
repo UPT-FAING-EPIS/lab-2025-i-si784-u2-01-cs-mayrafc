b3f6609552c2e4a55145ceabf90d277bbfa0bced
mayrafc <mc2021072620@virtual.upt.pe>
2025-05-24 18:33:21 -0500

    correcion del test

--- MyMath/Math.Tests/RooterTests.cs
+++ MyMath/Math.Tests/RooterTests.cs
@@ -43,7 +43,7 @@ namespace Math.Tests
             }
             catch (ArgumentOutOfRangeException ex)
             {
-                StringAssert.Contains(ex.Message, "El valor ingresado es inválido, solo se puede ingresar números positivos");
+                StringAssert.Contains(ex.Message, "El valor ingresado es inválido, solo se puede ingresar números positivos.");
             }
         }
 
