f5d110ff8a8f720899d843df588fe275dfee7219
mayrafc <mc2021072620@virtual.upt.pe>
2025-05-24 09:21:26 -0500

    prueba de la seguda actividad actividad

--- MyMath/Math.Lib/Rooter.cs
+++ MyMath/Math.Lib/Rooter.cs
@@ -2,8 +2,19 @@ using System;
 
 namespace Math.Lib
 {
+    /// <summary>
+    /// Proporciona métodos matemáticos personalizados.
+    /// </summary>
     public class Rooter
     {
+        /// <summary>
+        /// Calcula la raíz cuadrada de un número positivo utilizando el método de Newton-Raphson.
+        /// </summary>
+        /// <param name="input">Número positivo del que se desea calcular la raíz cuadrada.</param>
+        /// <returns>Raíz cuadrada aproximada del número proporcionado.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Se lanza si el número ingresado es cero o negativo.
+        /// </exception>
         public double SquareRoot(double input)
         {
             if (input <= 0.0)
@@ -15,10 +26,4 @@ namespace Math.Lib
             while (System.Math.Abs(previousResult - result) > result / 1000)
             {
                 previousResult = result;
-                result = result - (result * result - input) / (2 * result);
-            }
-
-            return result;
-        }
-    }
-}
+                result = result - (result
