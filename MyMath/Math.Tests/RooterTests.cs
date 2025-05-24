a97738bba4fa7056a82f40572ef520e72a3dfa47
mayrafc <mc2021072620@virtual.upt.pe>
2025-05-24 18:41:38 -0500

    tests

--- MyMath/Math.Tests/RooterTests.cs
+++ MyMath/Math.Tests/RooterTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Math.Lib;
+using System;
 
 namespace Math.Tests
 {
@@ -17,31 +18,36 @@ namespace Math.Tests
         }
 
         [TestMethod]
-        public void RooterTestNegativeInputx()
+        public void RooterTestNegativeInput()
         {
             Rooter rooter = new Rooter();
-            try
-            {
-                rooter.SquareRoot(-10);
-            }
-            catch (System.ArgumentOutOfRangeException)
-            {
-                return;
-            }
-            Assert.Fail();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => rooter.SquareRoot(-10));
         }
 
-       [TestMethod]
+        [TestMethod]
         public void RooterTestNegativeInputWithMessage()
         {
             Rooter rooter = new Rooter();
-
             var ex = Assert.ThrowsException<ArgumentOutOfRangeException>(() => rooter.SquareRoot(-5));
-            
             StringAssert.Contains(
                 ex.Message,
                 "El valor ingresado es invalido, solo se puede ingresar números positivos"
             );
+        }
+
+        [TestMethod]
+        public void RooterTestZeroInput()
+        {
+            Rooter rooter = new Rooter();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => rooter.SquareRoot(0));
+        }
+
+        [TestMethod]
+        public void RooterTestPositiveSmallNumber()
+        {
+            Rooter rooter = new Rooter();
+            double result = rooter.SquareRoot(0.0004);
+            Assert.AreEqual(0.02, result, 0.0001);
         }
     }
 }
